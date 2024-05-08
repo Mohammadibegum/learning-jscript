cf3169019fefb3b635b3a212d818ea04a68418af
Mohammadibegum <101379315+Mohammadibegum@users.noreply.github.com>
2024-05-08 04:43:12 +0000

    update

--- .devcontainer/basics-javascript/variabless.js
+++ .devcontainer/basics-javascript/variabless.js
@@ -3,3 +3,9 @@ let email="moham@gmail.com"
 var userName = "mohamm"
 City="doha"
 console.log(accountId)
+// accountId=7
+console.log(accountId);
+email = "moham@google.com"
+userName = "mohammadi"
+City ="dawha"
+console.table([accountId,email,userName,City])
