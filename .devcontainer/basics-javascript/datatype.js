47bbb3ff42b35ade63925ee92d7ad3afbab5935e
Mohammadibegum <101379315+Mohammadibegum@users.noreply.github.com>
2024-05-11 13:27:45 +0000

    update

---
+++ .devcontainer/basics-javascript/datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer versioon
+// alert(3+3) we are using node.js not browser
+console.log(3+3) // code readability should be high
+
+let name= "mohammadi" // string 
+
+let age = 27 
+
+// number =>2 to the power of 53 
+//bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value - empty
+// undefined =>
+// symbol => to find uniqueness
+// object 
+
+console.log(typeof age);
+console.log(typeof null); //type ia object 
+console.log(typeof undefined); // type is undefined
