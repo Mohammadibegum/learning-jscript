6a4911b836b1ed10f50ee3af69d7296a3843fa97
Mohammadibegum <101379315+Mohammadibegum@users.noreply.github.com>
2024-05-23 11:10:52 +0000

    comparision

---
+++ .devcontainer/basics-javascript/comparisionoperator.js
@@ -0,0 +1,21 @@
+// console.log(null>0); false
+// console.log(null==0);
+// console.log(null>=0); true
+// console.log(null<=0); true 
+
+// console.log(undefined>0); false for all unpredictable output
+// console.log(undefined>=0);
+// console.log(undefined==0);
+// console.log(undefined<=0);
+// console.log(undefined<0);
+// console.log(undefined<=0);
+
+// console.log("2"==2); // true it will convert 
+// let num=2;
+// console.log(num=="2");
+// console.log(typeof num);
+
+console.log("2" === 2);//  false its a strick check === will not only checks vaules but it also checks the data type and does not allow conversions
+let num = 2;
+console.log(num==="2");
+console.log(typeof num);
