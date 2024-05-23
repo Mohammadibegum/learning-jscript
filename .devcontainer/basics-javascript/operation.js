e4ab14ad93b48ba8df888770cac00c241d4f80ab
Mohammadibegum <101379315+Mohammadibegum@users.noreply.github.com>
2024-05-23 10:54:20 +0000

    basic operations topics

---
+++ .devcontainer/basics-javascript/operation.js
@@ -0,0 +1,40 @@
+let value =3
+let negvalue = -value
+//console.log(negvalue);
+//console.log(typeof negvalue);
+// output -3 and numbber .... 
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**5);
+// console.log(25/5);
+// console.log(25%5);
+
+// console.log("1"+2);
+// console.log("1"+"2"+2);
+// console.log(1+"2");
+// console.log(1+2+"2"); 
+
+// console.log(true); output true
+
+// console.log(+true); output 1
+
+// console.log(true+); not accepted it will through error 
+
+// console.log(+""); output 0 but not accepted
+
+let gamecounter = 1;
+
+gamecounter++;
+
+if(gamecounter>=0)
+    {
+        console.log(gamecounter++);
+    }
+    else 
+    {
+        console.log("NOT a valid value");
+    }
+
