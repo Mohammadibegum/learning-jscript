c472f069441af879c2a573346bb6c1ad9af4f6f9
Mohammadibegum <101379315+Mohammadibegum@users.noreply.github.com>
2024-05-23 09:53:19 +0000

    conversion of datatype

---
+++ .devcontainer/basics-javascript/conversionoperation.js
@@ -0,0 +1,27 @@
+// let score=33;
+
+//let score="33"
+
+//let score ="33abc"
+//let score = null
+//let score = true
+//let isloggedin=""
+let isloggedin = "moh"
+let isboolean = Boolean(isloggedin)
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueNumber = Number(score)
+
+//console.log(typeof valueNumber); //score is acutully not a number but javascript will convert its datatype to number due to Number(score) 
+
+//console.log(valueNumber);
+
+//console.log(typeof isboolean);
+//console.log(isboolean);
+
+let someNumber = 33;
+let strNumber = String(someNumber);
+console.log(strNumber);
+console.log(typeof strNumber);
