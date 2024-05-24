06d5018581096dda83f0cfba5dfc05fe2bf8983d
Mohammadibegum <101379315+Mohammadibegum@users.noreply.github.com>
2024-05-24 10:40:45 +0000

    stack and heap memory

--- .devcontainer/basics-javascript/datatypes-summary.js
+++ .devcontainer/basics-javascript/datatypes-summary.js
@@ -4,15 +4,15 @@
 // 1. premitives  call by value when ever we callthat data it will return a value 
 // 7 type: string , number , boolean, null, undefined, symbol, BigInt
 
-const num= 12;
-const value=12.3;
+// const num= 12;
+// const value=12.3;
 
-let temp=null;
-let useremail;
- const id= Symbol('123');
- const id2= Symbol('123');
+// let temp=null;
+// let useremail;
+//  const id= Symbol('123');
+//  const id2= Symbol('123');
 
- BigInt = 867374578367656567n;
+//  BigInt = 867374578367656567n;
 
 
 // Reference type or non premitives :  allocating the reference in memory
@@ -20,28 +20,55 @@ let useremail;
 
 // array
 
-const fruites = ['mango', 'apple','pineapple']; 
+// const fruites = ['mango', 'apple','pineapple']; 
 
 //object
 
-let persondetails= {
-    name : "mohammadi",
-    age : 28,
+// let persondetails= {
+//     name : "mohammadi",
+//     age : 28,
 
+// }
+
+// // function 
+
+// const myfunction = function(){
+//     console.log("hello world");
+
+// }
+
+
+// console.log(typeof useremail); // undefined
+// console.log(typeof temp); // object
+// console.log(typeof myfunction); // function
+// console.log(typeof persondetails); // object
+// console.log(typeof fruites); // object
+// console.log(typeof id); // symbol 
+// console.log(typeof BigInt); //bigint
+
+
+// ******************************************************************
+// stack and heap memory stack(primitives) , heap (nonpremitive)
+
+let myname = "mohammadi"
+let lastname = myname;
+lastname = "begum"
+
+console.log(myname);
+console.log(lastname);
+
+let user1 = {
+    name : "mohammadi begum",
+    email: "mohamm@gmail.com"
 }
+console.log(user1);
+let user2 = user1;
+user2.email = "mohammadi@google.com";
 
-// function 
-
-const myfunction = function(){
-    console.log("hello world");
-
-}
+// console.log("user1 details------>"+user1);  nonpredectable output it just shows the datatype not the actual values
+// console.log(user1 + "---->user1 details"); nonpredectable output it just shows the datatype not the actual values
+// console.log(user1+user2);  nonpredectable output it just shows the datatype not the actual values
+console.log(user2);
 
 
-console.log(typeof useremail); // undefined
-console.log(typeof temp); // object
-console.log(typeof myfunction); // function
-console.log(typeof persondetails); // object
-console.log(typeof fruites); // object
-console.log(typeof id); // symbol 
-console.log(typeof BigInt);
+
