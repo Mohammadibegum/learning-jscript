be50d9f7b8b561e79176b79bc3ffcd09e1216440
Mohammadibegum <101379315+Mohammadibegum@users.noreply.github.com>
2024-05-26 11:16:10 +0000

    maths and numbers

---
+++ .devcontainer/basics-javascript/numbers.js
@@ -0,0 +1,30 @@
+const score =400
+
+const balance = new Number(100.856);
+
+// console.log(balance);
+// console.log(score);
+// console.log(typeof (score)); 
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.round(23.87));
+console.log(Math.ceil(23.87));
+console.log(Math.floor(23.87));
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random(max-min+1))+min);
+
+
+
+
+
