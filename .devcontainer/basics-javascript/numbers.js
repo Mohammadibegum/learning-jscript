3c316ba31fcf6f21af9a8ca21ae8ef66bb18efaf
Mohammadibegum <101379315+Mohammadibegum@users.noreply.github.com>
2024-05-26 11:23:35 +0000

    maths num

--- .devcontainer/basics-javascript/numbers.js
+++ .devcontainer/basics-javascript/numbers.js
@@ -9,20 +9,23 @@ const balance = new Number(100.856);
 // console.log(balance.toString().length);
 // console.log(balance.toFixed(2));
 // console.log(balance.toPrecision(3));
+console.log(Number.MAX_SAFE_INTEGER);
 
 const hundreds = 1000000;
 // console.log(hundreds.toLocaleString('en-IN'));
 
-console.log(Math);
-console.log(Math.PI);
-console.log(Math.round(23.87));
-console.log(Math.ceil(23.87));
-console.log(Math.floor(23.87));
-console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.round(23.87));
+// console.log(Math.ceil(23.87));
+// console.log(Math.floor(23.87));
+// console.log(Math.floor(Math.random()*10)+1);
 
 const min=10;
 const max=20;
-console.log(Math.floor(Math.random(max-min+1))+min);
+// console.log(Math.floor(Math.random(max-min+1))+min);
+
+console.log(Math.random().max);
 
 
 
