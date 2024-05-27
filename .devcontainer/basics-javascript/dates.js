62bdf02c29f5a11dcbced373f17cc5bfbeb3267e
Mohammadibegum <101379315+Mohammadibegum@users.noreply.github.com>
2024-05-27 12:09:32 +0000

    dates n time in js

---
+++ .devcontainer/basics-javascript/dates.js
@@ -0,0 +1,22 @@
+
+// Dates
+let mydate = new Date();
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleTimeString());
+// // console.log(Date.UTC());
+// console.log(mydate.getMilliseconds());
+// console.log(mydate.getDate());
+// console.log(mydate.getDay());
+// console.log(mydate.getFullYear());
+// console.log(mydate.getTime());
+// console.log(mydate.getHours());
+// console.log(mydate.getTimezoneOffset());
+// console.log(mydate.getUTCDay());
+// console.log(mydate.getUTCDate());
+// console.log(mydate.getUTCFullYear());
+// console.log(Date.now());
+// console.log(mydate.valueOf());
+
+let myCreateDate = new Date(2024,9,27)
+console.log(myCreateDate.toDateString());
+console.log();
