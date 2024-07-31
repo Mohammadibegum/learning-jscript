a19026746bcf86bc4fa573910cd0879a99aefd2b
Mohammadibegum <101379315+Mohammadibegum@users.noreply.github.com>
2024-07-31 10:15:39 +0000

    committ

--- .devcontainer/basics-javascript/string.js
+++ .devcontainer/basics-javascript/string.js
@@ -1,35 +1,54 @@
-const name = "mohammadi"
-const id= 123
-// console.log(name+id+"values");   //not recommded instead of that use string interpolation &{variable}
+// const name = "mohammadi"
+// const id= 123
+// // console.log(name+id+"values");   //not recommded instead of that use string interpolation &{variable}
 
 
-console.log(`my name is ${name} and my id is ${id}`);
+// console.log(`my name is ${name} and my id is ${id}`);
 
 
 const gamename = new String ('chess') 
-console.log(gamename);
-// console.log(gamename[0]);
-// console.log(gamename.length);
-// //console.log(gamename.lastIndexOf);
-// console.log(gamename.toLowerCase());
-// console.log(gamename.charAt(2));
-// console.log(gamename.charCodeAt(0));
+// console.log(gamename);
+// // console.log(gamename[0]);
+// // console.log(gamename.length);
+// // //console.log(gamename.lastIndexOf);
+// // console.log(gamename.toLowerCase());
+// // console.log(gamename.charAt(2));
+// // console.log(gamename.charCodeAt(0));
 
-// const newstring1 = gamename.substring(0,3);
-// console.log(newstring1);
+ const newstring1 = gamename.substring(0,3);
+// // console.log(newstring1);
 
 
-// const newstring2 = gamename.slice(1,3);
-// console.log(newstring2);
+// // const newstring2 = gamename.slice(1,3);
+// // console.log(newstring2);
 
-// const string3 = new String('   moohhham ');
-// console.log(string3.trim());
+// // const string3 = new String('   moohhham ');
+// // console.log(string3.trim()); // trim
 
-const url = 'https://mohm.com/%20test'
+// const url = 'https://mohm.com/%20test'
 
-console.log(url.replace('%20','java')); // replace 
+// console.log(url.replace('%20','java')); // replace 
 
-console.log(url.includes('moham'));
-const game2 = 'carromboard'
-console.log(game2.split('%',2));
+// console.log(url.includes('moham'));
+// const game2 = 'carromboard'
+// console.log(game2.split('%',2));
+
+
+const city1 = new String("london");
+const city = new String('paris')
+// console.log(city);
+// console.log(city.trim());
+// console.log(city.charAt(5));
+// console.log(city.indexOf('o'));
+// console.log(city.length);
+// console.log(city.lastIndexOf('o'));
+// console.log(city.split('n',5));
+// console.log(city.search('d'));
+// console.log(city1.localeCompare('city'));
+// console.log(city.substring(2));
+// console.log(city.substring(-1,4));
+
+// console.log(city1.replace('n','  '));
+
+console.log(city1.split('n'));
 
